46760e04a91b02dcd5a1145527902a59890a1ba2
dperezbe <dperez@e-contact.cl>
2021-12-16 22:34:06 -0300

    pokemon

--- FUNDAMENTOS DE LA WEB/jQuery/Cuál es ese Pokémon/pokemon.js
+++ FUNDAMENTOS DE LA WEB/jQuery/Cuál es ese Pokémon/pokemon.js
@@ -1,28 +1,30 @@
-$(document).ready(function(){
-    const wrapper = $('#wrapper'); 
-    const data = $('#data');
+$(document).ready(function () {
+  const wrapper = $("#pokemones");
+  const data = $("#data");
 
-    for (let index = 1; index < 151; index++){
-        wrapper.append(`<img id =${index} src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${index}.png" alt="pokemon" />`)
-    }
+  for (let index = 1; index < 151; index++) {
+    wrapper.append(
+      `<img id =${index} src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${index}.png" alt="pokemon" />`
+    );
+  }
 
-    $("img").click(function(){
-        let id = $(this).attr('id');
-        $.get(`https://pokeapi.co/api/v2/pokemon/${id}`, function(e){
+  $("img").click(function () {
+    let id = $(this).attr("id");
+    $.get(`https://pokeapi.co/api/v2/pokemon/${id}`, function (e) {
+      let type_list = "";
+      for (let index = 0; index < e.types.length; index++) {
+        type_list = type_list + `<li>${e.types[index].type.name}</li>`;
+      }
 
-            let type_list = "";
-            for (let index = 0; index < e.types.length; index++) {
-                type_list = type_list + `<li>${e.types[index].type.name}</li>`
-            }
-
-            data.html(`
-            <img id =${id} src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${id}.png" alt="pokemon" />
+      data.html(`
             <ul>
-                <li>Height: ${e.height} </li>
-                <li>Weight: ${e.weight} </li>
-                ${type_list}
+                <h2>${e.name} </h2>
+                <img id =${id} src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${id}.png" alt="pokemon" />
+                <li><b>Types</b>${type_list} </li><br>
+                <li><b>Height:</b> ${e.height} </li><br>
+                <li><b>Weight:</b> ${e.weight} </li>
             </ul>
-            `)
-        })
-    })
-})
+            `);
+    });
+  });
+});
