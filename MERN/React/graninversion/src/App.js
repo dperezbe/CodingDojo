a94a31fe5a6bd5213ded5932695926d30a208975
dperezbe <dperez@e-contact.cl>
2022-01-01 20:38:16 -0300

    Gran inversión

---
+++ MERN/React/graninversion/src/App.js
@@ -0,0 +1,40 @@
+import logo from './logo.svg';
+import './App.css';
+import PersonCard from './components/PersonCard';
+
+function App() {
+  return (
+    <>
+    <PersonCard 
+      firstName = "Jane"
+      lastName = "Dow"
+      age = "45"
+      hairColor = "Black"
+    />
+
+    <PersonCard 
+      firstName = "John"
+      lastName = "Smith"
+      age = "88"
+      hairColor = "Brown"
+    />
+
+    <PersonCard 
+      firstName = "Millard"
+      lastName = "Fillmore"
+      age = "50"
+      hairColor = "Brown"
+    />
+
+    <PersonCard 
+      firstName = "Maria"
+      lastName = "Smith"
+      age = "62"
+      hairColor = "Brown"
+    />
+
+   </>
+  );
+}
+
+export default App;
